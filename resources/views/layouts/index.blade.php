25d679c0b3c846a3aaedfd9b40f27513ff37d0e1
Zhafransss <zafranfahrul13@gmail.com>
2023-11-07 23:43:25 +0700

    Praktikum 6

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col -md-6"></div>
+            <div class="col -md-6"></divS>
+        </div>
+
+    </div>
+</section>
